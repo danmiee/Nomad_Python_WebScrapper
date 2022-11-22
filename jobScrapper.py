d3c4090764a737a3d3a9744771dd3b2188f9ae41
JeongMin,Lee <113369989+danmiee@users.noreply.github.com>
2022-11-22 13:32:34 +0000

    5.1~5.5
    - BeautifulSoup 임포트
    - url 가져오기
    - http 상태코드 확인 후 html코드 가져오기
    - find_all 활용하여 html 코드 내 탐색
    - keywordArgument에 대해 알기

---
+++ jobScrapper.py
@@ -0,0 +1,16 @@
+from requests import get
+from bs4 import BeautifulSoup
+
+base_url = "https://weworkremotely.com/remote-jobs/search?term="
+search_term = "python"
+
+response = get(f"{base_url}{search_term}")
+if response.status_code != 200:
+  print("Can't request website")
+# beautifulsoup을 이용하여 html코드 탐색 find_all
+else:
+  soup = BeautifulSoup(response.text, 'html.parser')
+  # class가 jobs인 section 찾기
+  ## why class_? class는 python에서 이미 사용중
+  jobs = soup.find_all('section', class_="jobs")
+  # 해당 section의 ul에서 모든 li 찾기
---
+++ keywordArgs.py
@@ -0,0 +1,8 @@
+def say_hello(name, age):
+  print(f"Hello, {name} you are {age} years old")
+
+# positional argument
+say_hello("nico", 12)
+
+# keyword argument
+say_hello(age=12, name="nico")
