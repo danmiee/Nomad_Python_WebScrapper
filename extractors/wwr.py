853d0c4d172bd2e232c7180353229fd1be11261e
JeongMin,Lee <113369989+danmiee@users.noreply.github.com>
2022-11-26 03:34:50 +0000

    페이지 url 수정
      - &limit=50 삭제

--- extractors/wwr.py
+++ extractors/wwr.py
@@ -2,12 +2,13 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 def extract_wwr_jobs(keyword):
   options = Options()
   options.add_argument("--no-sandbox")
   options.add_argument("--disable-dev-shm-usage")
   browser = webdriver.Chrome(options=options)
-  browser.get(f"https://kr.indeed.com/jobs?q={keyword}&limit=50")
+  browser.get(f"https://kr.indeed.com/jobs?q={keyword}")
 
   results = []
   soup = BeautifulSoup(browser.page_source, "html.parser")
@@ -17,16 +18,16 @@ def extract_wwr_jobs(keyword):
     job_posts = job_section.find_all('li')
     job_posts.pop(-1)
     for post in job_posts:
-      anchors = post.find_all('a')    
+      anchors = post.find_all('a')
       anchor = anchors[1]
       link = anchor['href']
       company, kind, region = anchor.find_all('span', class_="company")
       title = anchor.find('span', class_='title')
       job_data = {
-        'link' : f"https://weworkremotely.com/{link}",
-        'company' : company.string, 
-        'region' : region.string, 
-        'position' : title.string
+        'link': f"https://weworkremotely.com/{link}",
+        'company': company.string,
+        'region': region.string,
+        'position': title.string
       }
       results.append(job_data)
   return results
