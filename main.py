bb602dee5918e9ee62ae1f412f589e560c6e5ed3
JeongMin,Lee <113369989+danmiee@users.noreply.github.com>
2022-12-08 12:00:37 +0000

    6.2~6.7 Render Template, Form, Args, For Loops, Pico
      - Render Template
        - main.py와 같은 위치에 templates 폴더 생성
        - templates 폴더에 html 파일 작성하고 main.py에서 부르기
    
      - Form : html  form태그 사용법
    
      - Args
        - template에서 변수 받을 때는 {{}}
    
      - For Loops
        - template에서 python 코드 작성할 때는 {% %} 사용
        - python 코드 작성 완료 후 {% endfor %} 필수 기재
    
      - Pico : simple css
        - <head> 영역에 link 추가
        - 추가로 작성하고 싶은 태그 사이트에서 가져와서 넣기

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request
+# render_template : flask가 templates를 찾게 함
+# 유저가 응답을 받기 전에 변수를 data로 변경
+# request : 브라우저에 가서 콘텐츠 요청하는 것을 의미
+from extractors.indeed import extract_indeed_jobs
+from extractors.wwr import extract_wwr_jobs
 
 # 플라스크 애플리케이션 생성
 app = Flask("JobScrapper")
 
+# request - springboot 어노테이션 역할
 @app.route("/")
 def home():
-  return 'hey there!'
+  # templates 폴더명 변경 불가 / main.py와 같은 위치에 존재해야함
+  # home.html 파일명 변경 가능 / render_template 안에는 동일하게 기재해야함
+  return render_template("home.html",name="nico")
+
+@app.route("/search")
+def search():
+  keyword = request.args.get("keyword")
+  indeed = extract_indeed_jobs(keyword)
+  wwr = extract_wwr_jobs(keyword)
+  jobs = indeed + wwr
+  return render_template("search.html", keyword = keyword, jobs = jobs)
+
 
 # 이 코드는 Replit에서 만든 VM에서 실행되므로 Replit에게 웹사이트 만들거니 접속을 열어달라고 알려주기
 app.run("0.0.0.0")
